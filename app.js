edfa638730bb936d1be9f11481ba87084cc0640b
suzz-in <o_o_su_jin_@naver.com>
2023-05-31 14:50:25 +0900

    init

---
+++ app.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.sendStatus(404);
+});
+
+app.use((error, req, res, next) => {
+  console.error(error);
+  res.sendStatus(500);
+});
